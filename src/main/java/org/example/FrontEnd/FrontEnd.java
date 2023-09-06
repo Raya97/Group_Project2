402f077c098010c1241c079b3c13fc61a21140ae
Raisa <vlasenkoraisa9723@gmail.com>
2023-09-06 15:46:09 +0200

    Initial commit

--- src/main/java/org/example/FrontEnd/FrontEnd.java
+++ src/main/java/org/example/FrontEnd/FrontEnd.java
@@ -1,6 +1,6 @@
 package org.example.FrontEnd;
+
 import org.example.GameBody.GameBody;
-import org.example.Parser.CityParser;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,72 +8,66 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
-import java.util.Set;
 
 public class FrontEnd implements ActionListener {
+    private static final int HEIGHT_FIRST = 100;
+    private static final int HEIGHT_REGULAR = 500;
+    private static final int WIDTH = 400;
+    private static final int LETTER_SIZE = 18;
     JFrame frame;
     JTextField textField;
     public static ArrayList<String> listOfCities = new ArrayList<>();
     int gamerCount = 0;
     int computerCount = 0;
-    Font defaultFont = new Font("Arial", Font.PLAIN, 18);  // Збільшений шрифт
+    Font defaultFont = new Font("Arial", Font.PLAIN, LETTER_SIZE);
 
     public FrontEnd() {
         frame = new JFrame("Гра в мітса");
-        frame.setSize(400, 100);
+        frame.setSize(WIDTH, HEIGHT_FIRST);
         frame.setLayout(new BorderLayout(40, 30));
-
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
         JLabel label = new JLabel("Вітаємо в грі в міста, бажаємо успіху!");
-        label.setFont(defaultFont);  // Застосування шрифту до label
+        label.setFont(defaultFont);
         frame.add(label, BorderLayout.NORTH);
-
-        textField = new JTextField();
-        textField.setFont(defaultFont);  // Застосування шрифту до textField
-        frame.add(textField, BorderLayout.CENTER);
-
         JButton button = new JButton("Старт");
-        button.setFont(defaultFont);  // Застосування шрифту до кнопки
         button.addActionListener(this);
+        button.setFont(defaultFont);
         frame.add(button, BorderLayout.SOUTH);
-
         JPanel panelEast = new JPanel();
         JPanel panelWest = new JPanel();
         frame.add(panelEast, BorderLayout.EAST);
         frame.add(panelWest, BorderLayout.WEST);
-
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(textField==null){
-            if(gamerCount==0){
+        if (textField == null) {
+            if (gamerCount == 0) {
                 firstPage();
-            }
-            else{
+            } else {
                 continuePage();
             }
-        }
-        else {
+        } else {
             String input = textField.getText().strip().toLowerCase();
-            String normalizedInput = input.replace(" ", "");
-
+            String city = GameBody.gameBody().getCity();
             if (!input.isBlank()) {
                 if (input.equals("здаюсь")) {
                     losingPage();
-                } else if (gamerCount != 0 && !String.valueOf(input.charAt(0)).equals(findLast(GameBody.gameBody().getCity()))) {
+                } else if (gamerCount != 0 && !String.valueOf(input.charAt(0)).equals(findLast(city))) {
                     brokenRules();
                 } else {
-                    if (listOfCities.contains(normalizedInput)) {
+                    if (listOfCities.contains(input)) {
                         usedCity();
-                    } else if (!GameBody.gameBody().getAllCities().contains(normalizedInput)) {
+                    } else if (!GameBody.gameBody().getAllCities().contains(input)) {
                         unknownCity();
                     } else {
                         String lastLetter = findLast(input);
                         GameBody.gameBody().setLastLetter(lastLetter);
                         gamerCount++;
-                        listOfCities.add(normalizedInput);
+                        listOfCities.add(input);
                         continuePage();
                     }
                 }
@@ -83,103 +77,116 @@ public class FrontEnd implements ActionListener {
         }
     }
 
-
-
-
-
-
-
-    public void usedCity(){
+    public void usedCity() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
         frame = new JFrame("Гра в мітса");
-        JLabel label = new JLabel("Вибачте, це місто вже було використано \n"+ "Будь ласка, введіть інше місто");
-        textField= new JTextField();
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Вибачте, це місто вже було використано \n" + "Будь ласка, введіть інше місто");
+        textField = new JTextField();
         JButton button = new JButton("Зробити хід");
         label.setFont(defaultFont);
         textField.setFont(defaultFont);
         button.setFont(defaultFont);
-        button.setFont(new Font("Arial", Font.BOLD, 18));
-        button.setBackground(Color.GREEN);
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));
+        button.setBackground(Color.RED);
         button.addActionListener(this);
         frame.add(label, BorderLayout.NORTH);
         frame.add(textField, BorderLayout.CENTER);
         frame.add(button, BorderLayout.SOUTH);
-        frame.setSize(720, 150);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void unknownCity(){
+    public void unknownCity() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
         frame = new JFrame("Гра в мітса");
-        JLabel label = new JLabel("Вибачте, я не знаю такого міста \n"+ "Будь ласка, введіть інше місто");
-        textField= new JTextField();
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Вибачте, я не знаю такого міста \n" + "Будь ласка, введіть інше місто");
+        textField = new JTextField();
         JButton button = new JButton("Зробити хід");
-        button.setFont(new Font("Arial", Font.BOLD, 18));
         button.setBackground(Color.RED);
         button.addActionListener(this);
         frame.add(label, BorderLayout.NORTH);
         label.setFont(defaultFont);
         textField.setFont(defaultFont);
-        button.setFont(defaultFont);
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));
         frame.add(textField, BorderLayout.CENTER);
         frame.add(button, BorderLayout.SOUTH);
-        frame.setSize(730, 150);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void losingPage(){
+    public void losingPage() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
         frame = new JFrame("Кінець");
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
         JLabel label = new JLabel("Ви програли, сподіваюсь вам сподобалось!");
+        label.setFont(defaultFont);
         frame.add(label, BorderLayout.CENTER);
-        frame.setSize(500, 150);
-        label.setFont(defaultFont);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-    }
-    public void firstPage(){
-        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-        frame = new JFrame("Гра в міста");
-        JLabel label = new JLabel("Введіть наступну назву міста");
-        textField= new JTextField();
-        JButton button = new JButton("Зробити хід");
-        button.setFont(new Font("Arial", Font.BOLD, 18));
-        button.setBackground(Color.GREEN);
-        button.addActionListener(this);
-        label.setFont(defaultFont);
-        textField.setFont(defaultFont);
-        button.setFont(defaultFont);
-        frame.add(button, BorderLayout.SOUTH);
-        frame.add(textField, BorderLayout.CENTER);
-        frame.add(label, BorderLayout.NORTH);
-        frame.setSize(400, 150);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void continuePage(){
-        String city = GameBody.gameBody().makeMove();
-        city = capitalizeFirstLetter(city);  // додатковий рядок коду
-        computerCount++;
+    public void firstPage() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
         frame = new JFrame("Гра в міста");
-        JLabel label = new JLabel("Комп'ютер: "+ city + "; Рахунок (комп'ютер:гравець) --" + computerCount + ":" + gamerCount);
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Введіть перше місто!");
         textField = new JTextField();
         JButton button = new JButton("Зробити хід");
-        button.setFont(new Font("Arial", Font.BOLD, 18));  // Жирний шрифт для кнопки
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));
         button.setBackground(Color.GREEN);
         button.addActionListener(this);
         label.setFont(defaultFont);
         textField.setFont(defaultFont);
-        button.setFont(defaultFont);
         frame.add(button, BorderLayout.SOUTH);
         frame.add(textField, BorderLayout.CENTER);
         frame.add(label, BorderLayout.NORTH);
-        frame.setSize(500, 150);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void continuePage() {
+        String city = GameBody.gameBody().makeMove();
+        if (city.equals("здаюсь")) computerLost();
+        city = capitalizeFirstLetter(city);
+        computerCount++;
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        frame = new JFrame("Гра в міста");
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Комп'ютер: " + city + "; Рахунок (комп'ютер:гравець) --" + computerCount + ":" + gamerCount);
+        textField = new JTextField();
+        JButton button = new JButton("Зробити хід");
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));  // Жирний шрифт для кнопки
+        button.setBackground(Color.GREEN);
+        button.addActionListener(this);
+        label.setFont(defaultFont);
+        textField.setFont(defaultFont);
+        frame.add(button, BorderLayout.SOUTH);
+        frame.add(textField, BorderLayout.CENTER);
+        frame.add(label, BorderLayout.NORTH);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public String capitalizeFirstLetter(String str) {
@@ -190,47 +197,68 @@ public class FrontEnd implements ActionListener {
         }
     }
 
-    public void noCityPage(){
+    public void computerLost() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-        frame = new JFrame("Гра в мітса");
-        JLabel label = new JLabel("Ведіть місто!");
-        textField= new JTextField();
-        JButton button = new JButton("Зробити хід");
-        button.setFont(new Font("Arial", Font.BOLD, 18));
-        button.setBackground(Color.GREEN);
-        button.addActionListener(this);
+        frame = new JFrame("Кінець");
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Вітаю, ви виграли! Сподіваюсь вам сподобалось!");
         label.setFont(defaultFont);
-        textField.setFont(defaultFont);
-        button.setFont(defaultFont);
-        frame.add(label, BorderLayout.NORTH);
-        frame.add(textField, BorderLayout.CENTER);
-        frame.add(button, BorderLayout.SOUTH);
-        frame.setSize(400, 150);
+        frame.add(label, BorderLayout.CENTER);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void brokenRules(){
+    public void noCityPage() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
         frame = new JFrame("Гра в мітса");
-        JLabel label = new JLabel("Ви порушили правила, місто повинно починатись з \n" +
-                "останньої літери попереднього ходу");
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Ви не ввели місто!");
+        textField = new JTextField();
         JButton button = new JButton("Зробити хід");
-        button.setFont(new Font("Arial", Font.BOLD, 18));
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));
         button.setBackground(Color.RED);
         button.addActionListener(this);
         label.setFont(defaultFont);
         textField.setFont(defaultFont);
-        button.setFont(defaultFont);
         frame.add(label, BorderLayout.NORTH);
         frame.add(textField, BorderLayout.CENTER);
         frame.add(button, BorderLayout.SOUTH);
-        frame.setSize(820, 150);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public String findLast(String city){
+    public void brokenRules() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        frame = new JFrame("Гра в мітса");
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.jpg");
+        frame.setIconImage(icon);
+        JLabel label = new JLabel("Ви порушили правила, місто повинно починатись з \n" +
+                "останньої літери попереднього ходу");
+        JButton button = new JButton("Зробити хід");
+        button.setFont(new Font("Arial", Font.BOLD, LETTER_SIZE));
+        button.setBackground(Color.RED);
+        button.addActionListener(this);
+        label.setFont(defaultFont);
+        textField.setFont(defaultFont);
+        frame.add(label, BorderLayout.NORTH);
+        frame.add(textField, BorderLayout.CENTER);
+        frame.add(button, BorderLayout.SOUTH);
+        frame.setSize(WIDTH, HEIGHT_REGULAR);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public String findLast(String city) {
         String lastLetter;
         if (city.charAt(city.length() - 1) == 'ь' || city.charAt(city.length() - 1) == 'и') {
             lastLetter = String.valueOf(city.charAt(city.length() - 2));
@@ -238,13 +266,5 @@ public class FrontEnd implements ActionListener {
             lastLetter = String.valueOf(city.charAt(city.length() - 1));
         }
         return lastLetter;
-    }
-
-
-    public static void main(String[] args) {
-        Set<String> citySet =new CityParser().parseCities() ;
-
-        GameBody.gameBody().setAllCities(citySet);
-        new FrontEnd();
     }
 }
